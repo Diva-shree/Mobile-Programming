4df2f442e8b9ef412f8d44e2c9f1cbf96a4c173e
Diva-shree <divashree.rana@westcliff.edu>
2026-03-24 08:26:22 +0545

    Animation

--- script.js
+++ script.js
@@ -8,19 +8,14 @@ let S5 = Number(document.getElementById("S5").value);
 let S6 = Number(document.getElementById("S6").value);
 let S7 = Number(document.getElementById("S7").value);
 let S8 = Number(document.getElementById("S8").value);
-
 let total = S1+S2+S3+S4+S5+S6+S7+S8;
-
 let totaltext = document.getElementById("total");
 let result = document.getElementById("result");
-
 totaltext.innerHTML="Total marks:" + total;
-
 if(S1>100 || S2>100){
     result.innerHTML="marks cannot be more than 100";
     return;
 }
-
 if(total < 400){
     result.innerHTML = "Fail";
     result.style.color = "Red";
